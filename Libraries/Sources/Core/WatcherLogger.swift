2a379e01b55590b6711320b77e00d87dcee68a1b
David bennett <david.a.bennett7@gmail.com>
2026-03-28 10:11:53 -0400

    Revert "Task 0: Define contracts & interfaces (docs/plans/voice-memo-watcher)"
    This reverts commit a0dd61adc95000021478baed229c2ed044a36bea.

--- Libraries/Sources/Core/WatcherLogger.swift
+++
@@ -1,7 +0,0 @@
-/// Abstracts logging so that tests can capture and assert on log messages
-/// instead of relying on os.Logger output.
-public protocol WatcherLogger: Sendable {
-    func info(_ message: String)
-    func warning(_ message: String)
-    func error(_ message: String)
-}
